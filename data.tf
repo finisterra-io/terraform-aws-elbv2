e1148bff1290af464e1764a9326e8ed42dd35caf
Daniel J <daniel@finisterra.io>
2024-02-21 09:17:50 -0500

    fix: data subnets

--- data.tf
+++ data.tf
@@ -7,11 +7,10 @@ data "aws_vpc" "default" {
 }
 
 data "aws_subnet" "default" {
-  count      = var.enabled && var.subnet_names != null ? length(var.subnet_names) : 0
-  vpc_id     = var.vpc_name != "" ? data.aws_vpc.default[0].id : var.vpc_id
-  cidr_block = lookup(var.subnet_names[count.index], "cidr_block", null)
+  count  = var.enabled && var.subnet_names != null ? length(var.subnet_names) : 0
+  vpc_id = var.vpc_name != "" ? data.aws_vpc.default[0].id : var.vpc_id
   filter {
     name   = "tag:Name"
-    values = [lookup(var.subnet_names[count.index], "name", "")]
+    values = [var.subnet_names[count.index]]
   }
 }
